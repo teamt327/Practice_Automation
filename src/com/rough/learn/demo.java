191da1586f253d759377e64fe7a81845b8e317c2
teamt327 <teamt327@gmail.com>
2019-01-17 18:41:12 +0530

    Added chrome driver

--- src/com/rough/learn/demo.java
+++ src/com/rough/learn/demo.java
@@ -1,6 +1,8 @@
 package com.rough.learn;
 
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class demo {
@@ -8,9 +10,26 @@ public class demo {
 	public static void main(String args[]) throws InterruptedException
 	{
 		System.out.println("Start the journey");
-		System.setProperty("webdriver.gecko.driver","E:\\Selenium\\Jar_Lib\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.gecko.driver","E:\\Selenium\\Jar_Lib\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		String key = "webdriver.chrome.driver";
+		String value = "E:\\Selenium\\Jar_Lib\\chrome\\chromedriver.exe";
+		System.setProperty(key, value);
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
+		Point p = new Point(350,250);
+		driver.manage().window().setPosition(p);
+	
+		Thread.sleep(1000);
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		driver.navigate().to("https://qa-cons-gy.halosys.com/MobileManagerPortal/Login");
+		Thread.sleep(2000);
+		String title = driver.getTitle();
+		System.out.println(title);
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+	
 		driver.close();
 	}
 
