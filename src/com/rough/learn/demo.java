a61b584613da70a89481bbceae2d0a8056cf2612
teamt327 <teamt327@gmail.com>
2019-01-23 17:51:58 +0530

    added chrome driver

--- src/com/rough/learn/demo.java
+++ src/com/rough/learn/demo.java
@@ -1,9 +1,17 @@
 package com.rough.learn;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByClassName;
+import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.XpiDriverService;
+import org.openqa.selenium.interactions.ClickAction;
 
 public class demo {
 	
@@ -16,21 +24,37 @@ public class demo {
 		String value = "E:\\Selenium\\Jar_Lib\\chrome\\chromedriver.exe";
 		System.setProperty(key, value);
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://google.com");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.MINUTES);
+		/*driver.get("https://google.com");
 		Point p = new Point(350,250);
 		driver.manage().window().setPosition(p);
 	
 		Thread.sleep(1000);
 		driver.manage().window().maximize();
 		Thread.sleep(1000);
-		driver.navigate().to("https://qa-cons-gy.halosys.com/MobileManagerPortal/Login");
-		Thread.sleep(2000);
+*/		
+		driver.get("https://qa-cons-gy.halosys.com/MobileManagerPortal/Login");
 		String title = driver.getTitle();
 		System.out.println(title);
 		String url = driver.getCurrentUrl();
 		System.out.println(url);
-	
-		driver.close();
+		WebElement email = driver.findElement(By.id("inputEmail"));
+		if(email.isEnabled())
+		{
+			System.out.println("Its enabled");
+			email.sendKeys("sonata;;p");
+		}
+		else
+		{
+			System.out.println("Its disabled");
+		}
+		driver.findElement(By.id("inputPassword")).sendKeys("sonata");
+		System.out.println("PAssword is entered");
+ 		driver.findElement(By.xpath("//button[@type='submit']")).click();
+ 		System.out.println("Logged in successfully");
+		//System.out.println(element.toString());
+		
+		//driver.close();
 	}
 
 }
